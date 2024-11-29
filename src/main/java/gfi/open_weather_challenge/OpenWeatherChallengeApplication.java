539c35a70b0b4bc4edab9061ce538c21208d5d5e
Armando Cervantes <armandocervantes6@gmail.com>
2024-11-29 14:12:23 -0600

    creating spring project

---
+++ src/main/java/gfi/open_weather_challenge/OpenWeatherChallengeApplication.java
@@ -0,0 +1,13 @@
+package gfi.open_weather_challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenWeatherChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenWeatherChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gfi/open_weather_challenge/OpenWeatherChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package gfi.open_weather_challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenWeatherChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
